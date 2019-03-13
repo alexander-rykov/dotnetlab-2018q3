58f2d481fa87d271e006da69951f80eb05314b1c
Yury Kupreyeu <yury_kupreyeu@thomsonreuters.com>
2019-03-14 00:41:14 +0300

    added customer service impl

---
+++ Logging, audit/LoggingSample/LoggingSample_BLL/Services/CustomerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using LoggingSample_BLL.Helpers;
+using LoggingSample_BLL.Models;
+using LoggingSample_DAL.Context;
+
+namespace LoggingSample_BLL.Services
+{
+    public class CustomerService : IDisposable
+    {
+        private readonly AppDbContext _context = new AppDbContext();
+
+        public Task<CustomerModel> GetCustomer(int customerId)
+        {
+            return _context.Customers.SingleOrDefaultAsync(item => item.Id == customerId).ContinueWith(task => {
+                var customer = task.Result;
+
+                return customer?.Map();
+            });
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
